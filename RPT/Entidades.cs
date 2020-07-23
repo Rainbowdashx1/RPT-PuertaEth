f8e7c6567642e7deba52035306081fd0b5cb5de9
RainBowDash <pablo.venegas@wisetrack.cl>
2020-07-23 07:02:40 -0400

    Agregar archivos de proyecto.

---
+++ RPT/Entidades.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPT
+{
+    public class Entidades
+    {
+
+        public class ObjetoPosiciones 
+        {
+            public string ID { get; set; }
+            public string Onu { get; set; }
+            public string OperStatus { get; set; }
+            public string ConfigState { get; set; }
+            public string DownloadState { get; set; }
+            public string Tx { get; set; }
+            public string Rx { get; set; }
+            public string KM { get; set; }
+            public string OnuStatus { get; set; }
+            public string State { get; set; }
+        }
+        public class ObjScaner 
+        {
+            public string Onu { get; set; }
+            public string ConfiguredAutoDetection { get; set; }
+            public string AdministrativeState { get; set; }
+            public string OperationalState { get; set; }
+            public string ConnectionType { get; set; }
+        }
+
+    }
+}
---
+++ RPT/Mensajes.cs
@@ -0,0 +1,32 @@
+﻿
+namespace RPT
+{
+    public class Mensajes
+    {
+        public string MensajeEncabezado() 
+        {
+            return ""
+                  +"██████╗░██████╗░████████╗\n"
+                  + "██╔══██╗██╔══██╗╚══██╔══╝\n"
+                  + "██████╔╝██████╔╝░░░██║░░░\n"
+                  + "██╔══██╗██╔═══╝░░░░██║░░░\n"
+                  + "██║░░██║██║░░░░░░░░██║░░░\n"
+                  + "╚═╝░░╚═╝╚═╝░░░░░░░░╚═╝░░░\n";
+        }
+
+        public string MenuInicial()
+        {
+            return "**************************\n"
+                   + "1.- Ingresar Ip para extracción\n"
+                   + "**************************";
+        }
+
+        public string IpSinFormato()
+        {
+            return "**************************\n"
+                   + "Ip no tiene formato correcto ingrese otra \n"
+                   + "**************************";
+        }
+
+    }
+}
